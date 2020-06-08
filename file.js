b9cc90be060043cf213db466993e68b89810971f
KatieCollier <katie.collier@hotmail.fr>
2020-06-08 10:24:46 +0200

    Insert product info on vue page with JS

--- file.js
+++ file.js
@@ -1,19 +1,44 @@
+/* Create request to receive information about teddies from the server*/
 const request = new XMLHttpRequest();
 const url='http://localhost:3000/api/teddies';
 request.open("GET", url);
 request.send();
 
 request.onreadystatechange = function(){
+  /* If the request works, retrieve, parse and store information in the response variable*/
   if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
       var response = JSON.parse(request.responseText);
-      console.log(response);
-      var teddyName = document.getElementById("teddy-name");
-      teddyName.innerHTML = response[0].name;
-      console.log(teddyName);
-      var teddyImg = document.getElementById("teddy-img");
-      teddyImg.innerHTML = "src=\"" + response[0].imageUrl +"\" alt=\"\"" ;
-      console.log(teddyImg);
-      console.log(response[0].imageUrl);
+      /* for each teddy in the response, extract and name in a variable its name, price, image url, description and colors*/
+      for (var i = 0; 1 < response.length; i++){
+        let teddy = response[i];
+        let teddyInfo = {
+          teddyName: teddy.name,
+          teddyPrice: teddy.price,
+          teddyUrl: teddy.imageUrl,
+          teddyDescription: teddy.description,
+          teddyColors: teddy.colors
+        }
+        /*create an array to store the TeddyInfo objects in */
+        var Teddies = [];
+        /* add the TeddyInfo object to the array*/
+        Teddies.push(teddyInfo);
+
+        /*create new card with the necessary information for the vue page*/
+        const main = document.getElementById("main-vue");
+
+        const newCard = document.createElement("div");
+        main.appendChild(newCard);
+        /* add the content from the server to the new card*/
+        newCard.innerHTML = '<div class="card"\>\
+                              <div class="card-body"\>\
+                                <img class="card-img-top" src="' + teddyInfo.teddyUrl + '" alt=""\>\
+                                <div class="card-text"\>\
+                                  <p>' +  teddyInfo.teddyName + '</p\>\
+                                  <p>' +  teddyInfo.teddyPrice + '</p\>\
+                                </div\>\
+                              </div\>\
+                            </div\>'
+      }
   }
 }
 
