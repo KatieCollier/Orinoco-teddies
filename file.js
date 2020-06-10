bd429e25224e1f4b7494a7eeb6ae9fdde2877adb
KatieCollier <katie.collier@hotmail.fr>
2020-06-10 08:53:38 +0200

    JS fill in vue page cards, store clicked card nb

--- file.js
+++ file.js
@@ -1,46 +1,58 @@
-/* Create request to receive information about teddies from the server*/
-const request = new XMLHttpRequest();
-const url='http://localhost:3000/api/teddies';
-request.open("GET", url);
-request.send();
+/* create a function containing a request to retrieve teddy data */
+var getInfo = function(url, success){
+  var request = new window.XMLHttpRequest();
 
-request.onreadystatechange = function(){
-  /* If the request works, retrieve, parse and store information in the response variable*/
-  if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
-      var response = JSON.parse(request.responseText);
-      /* for each teddy in the response, extract and name in a variable its name, price, image url, description and colors*/
-      for (var i = 0; 1 < response.length; i++){
-        let teddy = response[i];
-        let teddyInfo = {
-          teddyName: teddy.name,
-          teddyPrice: teddy.price,
-          teddyUrl: teddy.imageUrl,
-          teddyDescription: teddy.description,
-          teddyColors: teddy.colors
-        }
-        /*create an array to store the TeddyInfo objects in */
-        var Teddies = [];
-        /* add the TeddyInfo object to the array*/
-        Teddies.push(teddyInfo);
-
-        /*create new card with the necessary information for the vue page*/
-        const main = document.getElementById("main-vue");
-
-        const newCard = document.createElement("div");
-        main.appendChild(newCard);
-        /* add the content from the server to the new card*/
-        newCard.innerHTML = '<div class="card m-2"\>\
-                              <div class="card-body pb-0"\>\
-                                <div class="img-size">\
-                                <img class="card-img-top mb-2" src="' + teddyInfo.teddyUrl + '" alt=""\>\
-                                </div>\
-                                <div class="card-text row mb-0 py-2"\>\
-                                  <h2 class="col my-auto">' +  teddyInfo.teddyName + '</h2\>\
-                                  <p class="col text-right my-auto ml-auto">' +  teddyInfo.teddyPrice + ' € </p\>\
-                                </div\>\
-                              </div\>\
-                            </div\>'
-      }
+  request.onreadystatechange = function(){
+      if(this.readyState == 4){
+          success(request.responseText);
   }
 }
+request.open("GET", url, true );
+request.send();
+}
 
+/*create a function that only runs once the getInfo function has been completed
+This function creates a new card for each teddy and fills it with the corresponding data*/
+var createCard = function () {
+  getInfo('http://localhost:3000/api/teddies', function (response) {
+      var Teddies = JSON.parse(response);
+      const main = document.getElementById("main-vue");
+      for (let i = 0; i < Teddies.length; i++) {/* for each teddy in the server*/
+          /*create new card with the necessary information for the vue page*/
+          const newCard = document.createElement("div");
+          main.appendChild(newCard);
+          /* add the content from the server to the new card*/
+          newCard.innerHTML = '<div class="card m-2"\>\
+                                <div class="card-body pb-0"\>\
+                                  <div class="img-size">\
+                                    <img class="card-img-top mb-2" src="' + Teddies[i].imageUrl + '" alt=""\>\
+                                  </div>\
+                                  <div class="card-text row mb-0 py-2"\>\
+                                    <a id="card-' + i + '"class="stretched-link" href="product.html">\
+                                      <h2 class="col my-auto">' +  Teddies[i].name + '</h2\>\
+                                    </a>\
+                                    <p class="col text-right my-auto ml-auto">' +  Teddies[i].price + ' € </p\>\
+                                  </div\>\
+                                </div\>\
+                              </div\>';
+      }
+  });
+}
+
+/* Create a function to identify which teddy is clicked on in the vue page
+Save teddy number to session storage for future use in filling out product page*/
+const goToProduct = function(){
+  const main = document.getElementById("main-vue");
+  main.addEventListener("click", function(event){
+    console.log(event.target.id, "was clicked!");
+    let j = parseInt(event.target.id.replace("card-",""));
+    console.log(j);
+    sessionStorage.setItem("clickedTeddy", j);
+  })
+}
+
+/*run the create card function only on the vue page*/
+if (document.getElementById("main-vue")){
+  createCard();
+  goToProduct();
+}
